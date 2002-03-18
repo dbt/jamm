7bc7a6b934939d7a65b1d87b01656b4323ddfe2b
Dave Dribin <dave@dribin.org>
2002-03-18 22:20:23 +0000

    Moved to src/backend and updated to jamm.schema

---
+++ src/backend/test/java/jamm/ldap/AllTests.java
@@ -0,0 +1,19 @@
+package jamm.ldap;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests
+{
+    public static Test suite()
+    {
+        TestSuite suite;
+
+        suite = new TestSuite();
+        suite.addTestSuite(CryptPasswordTest.class);
+        suite.addTestSuite(Md5PasswordTest.class);
+        suite.addTestSuite(ShaPasswordTest.class);
+        suite.addTestSuite(LdapFacadeTest.class);
+        return suite;
+    }
+}
