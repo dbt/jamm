986cc653068b832c09b12a3668f91557ed98ee87
Dave Dribin <dave@dribin.org>
2002-04-02 23:23:03 +0000

    Added logout action and forward

---
+++ src/webapp/java/jamm/webapp/LogoutAction.java
@@ -0,0 +1,31 @@
+package jamm.webapp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.RedirectingActionForward;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionError;
+
+public class LogoutAction extends Action
+{
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm actionForm,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception
+    {
+        HttpSession session = request.getSession();
+        session.removeAttribute("is_authenticated");
+        session.removeAttribute("user");
+        return (mapping.findForward("home"));
+    }
+}
