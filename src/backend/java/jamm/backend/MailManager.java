2142c3e73a43cdbb55e9fec0a8e5e7217a9354d3
Dave Dribin <dave@dribin.org>
2002-04-24 23:53:17 +0000

    Removed unused import

--- src/backend/java/jamm/backend/MailManager.java
+++ src/backend/java/jamm/backend/MailManager.java
@@ -5,7 +5,6 @@ import java.util.Set;
 import java.util.List;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
