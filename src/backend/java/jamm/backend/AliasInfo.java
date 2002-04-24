7419a73ab9d254e8609fada920d9610ced725be7
Dave Dribin <dave@dribin.org>
2002-04-24 21:18:09 +0000

    Moved value object to backend

---
+++ src/backend/java/jamm/backend/AliasInfo.java
@@ -0,0 +1,42 @@
+package jamm.backend;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+
+public class AliasInfo implements Serializable
+{
+    public AliasInfo(String name, List destinations, boolean active,
+                       boolean administrator)
+    {
+        mName = name;
+        mDestinations = destinations;
+        mActive = active;
+        mAdministrator = administrator;
+    }
+
+    public String getName()
+    {
+        return mName;
+    }
+
+    public List getDestinations()
+    {
+        return mDestinations;
+    }
+
+    public boolean isActive()
+    {
+        return mActive;
+    }
+
+    public boolean isAdministrator()
+    {
+        return mAdministrator;
+    }
+
+    private String mName;
+    private List mDestinations;
+    private boolean mActive;
+    private boolean mAdministrator;
+}
