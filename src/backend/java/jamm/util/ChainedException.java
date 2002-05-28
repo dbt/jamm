58906a4b23c97ca744a1ea85ecc45282d0719a67
Dave Dribin <dave@dribin.org>
2002-05-28 16:47:04 +0000

    Added javadoc for jamm.util

--- src/backend/java/jamm/util/ChainedException.java
+++ src/backend/java/jamm/util/ChainedException.java
@@ -17,55 +17,71 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 package jamm.util;
 
+/**
+ * Extends <code>Exception</code> to provide a root cause exception.
+ * This allows exceptions to be rethrown as a different exception, yet
+ * keep all the original exception information.
+ */
 public class ChainedException extends Exception
 {
-    private Throwable cause = null;
-
+    /**
+     * Create a new instance with no message and no root cause.
+     */
     public ChainedException()
     {
         super();
     }
 
+    /**
+     * Create a new instance with a message, but no root cause.
+     *
+     * @param message Message of this exception
+     */
     public ChainedException(String message)
     {
         super(message);
     }
 
+    /**
+     * Create a new instance with a message and a root cause.
+     *
+     * @param message Message of this exception
+     * @param cause Root cause of this exception
+     */
     public ChainedException(String message, Throwable cause)
     {
         super(message);
         this.cause = cause;
     }
 
+    /**
+     * Create a new instance with a root cause, but no message.
+     *
+     * @param cause Root cause of this exception
+     */
     public ChainedException(Throwable cause)
     {
         super();
         this.cause = cause;
     }
 
+    /**
+     * Gets the root cause of this exception. <code>null</code> is
+     * returned if there is no cause.
+     *
+     * @return The root cause of this exception.
+     */
     public Throwable getCause()
     {
         return cause;
     }
 
+    /**
+     * Prints the full stack trace, including the trace of the cause,
+     * to standard error.
+     */
     public void printStackTrace()
     {
         super.printStackTrace();
@@ -76,6 +92,12 @@ public class ChainedException extends Exception
         }
     }
 
+    /**
+     * Prints the full stack trace, including the trace of the cause,
+     * to a PrintStream.
+     *
+     * @param ps PrintStream to print to
+     */
     public void printStackTrace(java.io.PrintStream ps)
     {
         super.printStackTrace(ps);
@@ -86,6 +108,12 @@ public class ChainedException extends Exception
         }
     }
 
+    /**
+     * Prints the full stack trace, including the trace of the cause,
+     * to a PrintWriter.
+     *
+     * @param pw PrintWriter to print to
+     */
     public void printStackTrace(java.io.PrintWriter pw)
     {
         super.printStackTrace(pw);
@@ -95,4 +123,7 @@ public class ChainedException extends Exception
             cause.printStackTrace(pw);
         }
     }
+
+    /** The root cause of this exception. */
+    private Throwable cause = null;
 }
