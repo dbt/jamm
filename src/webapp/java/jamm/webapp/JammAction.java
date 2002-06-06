ccf1e2d42d42a7e82582ab207e0e12c5556429a0
Dave Dribin <dave@dribin.org>
2002-06-06 16:51:17 +0000

    Added findForward()

--- src/webapp/java/jamm/webapp/JammAction.java
+++ src/webapp/java/jamm/webapp/JammAction.java
@@ -19,10 +19,15 @@
 
 package jamm.webapp;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 
 import jamm.backend.MailManager;
 
@@ -61,5 +66,89 @@ public abstract class JammAction extends Action
                                Globals.getLdapSearchBase(),
                                user.getDn(),
                                user.getPassword());
+    }
+
+    /**
+     * Finds the forward of the specified name in the specified, but
+     * adds the required parameters for the forward.  This is due to
+     * the fact that a Struts <code>ActionForward</code> does not have
+     * any mechanism itself for adding parameters to a forward.
+     *
+     * @param mapping Mapping to use when finding the forward.
+     * @param name Name of the forward to find.
+     * @param request Will be used to grab parameters for certain actions.
+     * @return An instance to the specified forward.
+     */
+    protected ActionForward findForward(ActionMapping mapping, String name,
+                                        HttpServletRequest request)
+    {
+        ActionForward forward = mapping.findForward(name);
+        if (name.equals("domain_admin"))
+        {
+            String domain = request.getParameter("domain");
+            if (domain != null)
+            {
+                return addParameter(forward, "domain", domain);
+            }
+        }
+        else if (name.equals("account_admin"))
+        {
+            String mail = request.getParameter("mail");
+            if (mail != null)
+            {
+                return addParameter(forward, "mail", mail);
+            }
+        }
+
+        return forward;
+    }
+
+    /**
+     * Adds multiple parameters to the specified forward.  Each entry
+     * in the map is added as "key=value".
+     *
+     * @param forward The forward to add parameters to
+     * @param parameters The parameters to add
+     * @return A new forward with parameters.
+     */
+    protected ActionForward addParameters(ActionForward forward,
+                                          Map parameters)
+    {
+        if (!parameters.isEmpty())
+        {
+            StringBuffer path = new StringBuffer();
+            path.append(forward.getPath()).append("?");
+            Iterator entries = parameters.entrySet().iterator();
+            while (entries.hasNext())
+            {
+                Entry entry = (Entry) entries.next();
+                path.append(entry.getKey()).append("=");
+                path.append(entry.getValue());
+            }
+            return new ActionForward(path.toString(), forward.getRedirect());
+        }
+        else
+        {
+            return forward;
+        }
+    }
+
+    /**
+     * Adds a single parameter to the specified forward.
+     *
+     * @param forward The forward to add a parameter to
+     * @param parameterName The name of the parameter to add
+     * @param parameterValue The value of the parameter to add
+     * @return A new forward with a parameter.
+     */
+    protected ActionForward addParameter(ActionForward forward,
+                                         String parameterName,
+                                         String parameterValue)
+    {
+        StringBuffer path = new StringBuffer();
+        path.append(forward.getPath()).append("?");
+        path.append(parameterName).append("=");
+        path.append(parameterValue);
+        return new ActionForward(path.toString(), forward.getRedirect());
     }
 }
