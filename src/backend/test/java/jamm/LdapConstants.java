6a290b0f2217ac549a423f9b88efea6ac8961875
Dave Dribin <dave@dribin.org>
2002-06-18 22:34:06 +0000

    Add data for account 1

--- src/backend/test/java/jamm/LdapConstants.java
+++ src/backend/test/java/jamm/LdapConstants.java
@@ -26,10 +26,30 @@ public class LdapConstants
 {
     /** LDAP server: <code>localhost</code> */
     public static final String HOST = "localhost";
+
     /** LDAP port: 389 */
     public static final int PORT = 389;
+
     /** Root DN: <code>cn=Manager,dc=jamm,dc=test</code> */
     public static final String MGR_DN = "cn=Manager,dc=jamm,dc=test";
+
     /** Root password: <code>jammtest</code> */
     public static final String MGR_PW = "jammtest";
+
+    /**
+     * DN for account 1: <code>mail=acct1@domain1.test,
+     * jvd=domain1.test, o=hosting, dc=jamm, dc=test</code>
+     */
+    public static final String ACCT1_DN =
+        "mail=acct1@domain1.test,jvd=domain1.test,o=hosting,dc=jamm,dc=test";
+
+    /** Passowrd for account 1: <code>acct1pw</code> */
+    public static final String ACCT1_PW = "acct1pw";
+
+    /**
+     * Hashed password for account 1:
+     * <code>{SSHA}tk3w4vV6xghX4r7P0F1EAeA55jo53sSO</code>
+     */
+    public static final String ACCT1_PW_HASHED =
+        "{SSHA}tk3w4vV6xghX4r7P0F1EAeA55jo53sSO";
 }
