92816f1782e21b99b2a99f352f331320b81da236
Keith T. Garner <kgarner@kgarner.com>
2002-08-06 19:53:08 +0000

    Moved ModifyPasswordRequest out of LdapFacade

--- src/backend/java/jamm/ldap/LdapFacade.java
+++ src/backend/java/jamm/ldap/LdapFacade.java
@@ -29,7 +29,6 @@ import javax.naming.Context;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.AuthenticationException;
-import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchResult;
 import javax.naming.directory.SearchControls;
@@ -40,8 +39,6 @@ import javax.naming.directory.BasicAttribute;
 import javax.naming.ldap.LdapContext;
 import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.Control;
-import javax.naming.ldap.ExtendedRequest;
-import javax.naming.ldap.ExtendedResponse;
 
 import jamm.util.CaseInsensitiveStringSet;
 
@@ -624,105 +621,6 @@ public class LdapFacade
             context.extendedOperation(mpr);
     }
 
-    /**
-     * A class conforming to the ExtendedRequest that implements the
-     * Modify Password extended operation.
-     *
-     * @see javax.naming.ldap.ExtendedRequest
-     */
-    class ModifyPasswordRequest
-        implements ExtendedRequest
-    {
-        /**
-         * Creates a new <code>ModifyPasswordRequest</code> instance.
-         *
-         * @param dn the dn whose password is to change
-         * @param password the new password
-         */
-        public ModifyPasswordRequest(String dn, String password)
-        {
-            mDn = dn;
-            mPassword = password;
-        }
-        
-        /**
-         * Returns the ID of this extended operation.
-         *
-         * @return a String with the OID of this operation
-         */
-        public String getID()
-        {
-            return LDAP_EXOP_X_MODIFY_PASSWD;
-        }
-
-        /**
-         * Get the BER encoded value for this operation.
-         *
-         * @return a bytearray containing the BER sequence.
-         */
-        public byte[] getEncodedValue()
-        {
-            byte[] password = mPassword.getBytes();
-            byte[] dn = mDn.getBytes();
-
-            // Sequence tag (1) + sequence length (1) + dn tag (1) +
-            // dn length (1) + dn (variable) + password tag (1) +
-            // password length (1) + password (variable)
-            int encodedLength = 6 + dn.length + password.length;
-
-            byte[] encoded = new byte[encodedLength];
-
-            int i = 0;
-            encoded[i++] = (byte) 0x30; // sequence start
-            // length of body
-            encoded[i++] = (byte) (4 + dn.length + password.length);
-            
-
-            encoded[i++] = LDAP_TAG_EXOP_X_MODIFY_PASSWD_ID;
-            encoded[i++] = (byte) dn.length;
-
-            System.arraycopy(dn, 0, encoded, i, dn.length);
-            i += dn.length;
-
-            encoded[i++] = LDAP_TAG_EXOP_X_MODIFY_PASSWD_NEW;
-            encoded[i++] = (byte) password.length;
-
-            System.arraycopy(password, 0, encoded, i, password.length);
-            i += password.length;
-
-            return encoded;
-        }
-
-        /**
-         * Creates the extended response.  With OpenLDAP, the extended
-         * operation for Password modification doesn't create a
-         * response so we just return null here.
-         *
-         * @param id the OID of the response
-         * @param berValue the BER encoded value of the response
-         * @param offset the offset
-         * @param length the length of the response
-         * @return returns null as the modify password operation doesn't
-         *         generate a response.
-         * @exception NamingException if an error occurs
-         */
-        public ExtendedResponse createExtendedResponse(String id,
-                                                       byte[] berValue,
-                                                       int offset, int length)
-            throws NamingException
-        {
-            return null;
-        }
-
-        public static final String LDAP_EXOP_X_MODIFY_PASSWD =
-            "1.3.6.1.4.1.4203.1.11.1";
-        public static final byte LDAP_TAG_EXOP_X_MODIFY_PASSWD_ID =
-            (byte) 0x80;
-        public static final byte LDAP_TAG_EXOP_X_MODIFY_PASSWD_NEW =
-            (byte) 0x82;
-        private String mDn;
-        private String mPassword;
-    }
 
     /** Host name to connect to. */
     private String mHost;
